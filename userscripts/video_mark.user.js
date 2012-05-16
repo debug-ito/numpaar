bb0573b49256e2d3188e1a5ad8e7ba055699c37d
Toshio Ito <debug.ito@gmail.com>
2012-05-16 21:56:48 +0900

    Add userscripts/video_mark.user.js from numpaar-extensions-debug-ito, because this mainly belongs to YouTube and NicoLive Engines.

---
+++ userscripts/video_mark.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name          Video Marked
+// @namespace     http://debugitos.main.jp/
+// @description   Mark Video
+// @include       http://www.youtube.tld/watch*
+// @include       http://www.nicovideo.tld/watch/*
+// ==/UserScript==
+
+document.title = "[VIDEO] " + document.title;
+
+
